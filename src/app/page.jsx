7ffb121a3e59186435bd15f98628dbfb3dfe3f6f
Bea <ab291012@gmail.com>
2025-11-06 14:52:27 -0300

    feat: add jwt

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,3 +1,12 @@
-export default function Page() {
-	return null;
+'use client';
+import React from 'react';
+import Footer from "./components/Footer/Footer"
+
+export default function Home (){
+    return(
+        <>
+        <h1>Hello World</h1>
+        <Footer />
+    </>
+    )
 }
