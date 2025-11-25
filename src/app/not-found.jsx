215cf28d2019b12e2887dfabfe7678081cb5bdda
anna leme <162629023+annabialeme@users.noreply.github.com>
2025-11-25 14:00:29 -0300

    feat: create page notfound

--- src/app/not-found.jsx
+++ src/app/not-found.jsx
@@ -1,5 +1,27 @@
-export default function NotFound (){
-    return(
-        <h1>404 - Page Not Found</h1>
-    )
+import Image from "next/image";
+import Link from "next/link";
+import styles from './not-found.module.css';
+
+export default function NotFound() {
+    return (
+        <div className={styles.container}>
+            <h2 className={styles.subtitle}>Página não encontrada</h2>
+            <p className={styles.description}>
+                O conteúdo que você procura não está disponível ou foi movido.<br />
+                Volte para a página inicial ou tente novamente.
+            </p>
+
+            <Image
+                src="/images/gif.gif"
+                alt="Página não encontrada"
+                width={400}   
+                height={300}   
+                className={styles.image}
+            />
+
+            <Link href="/" className={styles.button}>
+                Voltar ao início
+            </Link>
+        </div>
+    );
 }
