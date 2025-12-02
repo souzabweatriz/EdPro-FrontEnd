b4cfead8f0d4818fb88667d43d473d929afe7773
Luana Domeneghetti <158210063+domeneghettii@users.noreply.github.com>
2025-12-02 11:36:30 -0300

    feat: correção middleware.js

--- src/middleware.js
+++ src/middleware.js
@@ -11,12 +11,12 @@ const JWT_CONFIG = {
 const protectedRoutes = {
     "/admin": ["admin"],
     "/alunos": ["aluno"],
-}
+};
 
 export async function middleware(request) {
-    const {pathname} = request.nextUrl;
+    const { pathname } = request.nextUrl;
 
-    const routeConfig = Object.entries(protectedRoutes).find(([route, roles]) => 
+    const routeConfig = Object.entries(protectedRoutes).find(([route, roles]) =>
         pathname.startsWith(route)
     );
 
@@ -25,27 +25,32 @@ export async function middleware(request) {
     }
 
     const token =
-    request.cookies.get("auth-token")?.value ||
-    request.headers.get("Authorization")?.slice(7);
+        request.cookies.get("auth-token")?.value ||
+        request.headers.get("Authorization")?.slice(7);
 
     if (!token) {
         return NextResponse.redirect(new URL("/login", request.url));
     }
+
     try {
-        const { payload } = await jwtVerify(token, JWT_SECRET,{
+        const { payload } = await jwtVerify(token, JWT_SECRET, {
             issuer: JWT_CONFIG.issuer,
             audience: JWT_CONFIG.audience,
-        })
+        });
+
         if (!routeConfig[1].includes(payload.role)) {
-            return NextResponse.redirect(new URL("/", request.url)); 
+            return NextResponse.redirect(new URL("/", request.url));
         }
-                return NextResponse.next();
+
+        return NextResponse.next();
     } catch (error) {
-            const response = NextResponse.redirect(new URL("/login", request.url));
+        console.error("Erro ao verificar o token JWT:", error.message);
+        const response = NextResponse.redirect(new URL("/login", request.url));
         response.cookies.delete("auth-token");
         return response;
     }
 }
+
 export const config = {
     matcher: ["/alunos/:path*", "/admin/:path*"],
 };
